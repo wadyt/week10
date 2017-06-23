0b298a2a418e1efccc177de8c8d0bd03101d9907
wadyt <wady.tupiza@gmail.com>
2017-06-22 21:57:38 -0500

    week 10 first repository

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express'),
+	path = require('path'),
+	bodyParser = require('body-parser'),
+	cons = require('consolidate'),
+	dust = require('dustjs-helpers'),
+	pg = require('pg'),
+	app = express();
+
+var connect = "postgress://wady:Chuggy@localhost/public.recipebookdb";
+
+app.engine('dust', cons.dust);
+
+app.set('view engine', 'dust');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function(req, res){
+      res.render('index');
+      
+      
+    });
+
+
+
+app.listen(3000, function(){
+	console.log('Server connected at Port 3000');
+});
+
